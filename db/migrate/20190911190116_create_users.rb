734871ba33bd54b990eed5488fc5bc4cd2ddab59
Gianmario Salvetti <gianmario.salvetti@gmail.com>
2019-09-11 21:03:12 +0200

    User Object Created

---
+++ db/migrate/20190911190116_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :firstname
+      t.string :lastname
+      t.string :password_digest
+
+      t.timestamps
+    end
+
+      # Create admin user
+      User.create(username: "admin", password: "admin")
+
+  end
+end
